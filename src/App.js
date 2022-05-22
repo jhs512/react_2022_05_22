e4afab51d14783c29ca87f3203246e164d16a7e8
jhs512 <jangka512@gmail.com>
2022-05-22 23:07:29 +0900

    MUI 설치

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Button } from "@mui/material";
+
 function App() {
-  return <div className="text-blue-600 text-4xl">안녕하세요.</div>;
+  return (
+    <div className="text-blue-600 text-4xl">
+      안녕하세요.
+      <Button variant="contained">버튼입니다.</Button>
+    </div>
+  );
 }
 
 export default App;
