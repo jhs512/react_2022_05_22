75b77d4e753bd65acc0d1d5800e0e5d2aa78ae39
jhs512 <jangka512@gmail.com>
2022-05-22 23:12:02 +0900

    MUI 테마 적용

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { Button } from "@mui/material";
 
 function App() {
   return (
-    <div className="text-blue-600 text-4xl">
-      안녕하세요.
+    <div>
+      <div className="text-blue-600 text-4xl">안녕하세요.</div>
+      <hr className="my-2" />
       <Button variant="contained">버튼입니다.</Button>
+      <hr className="my-2" />
+      <span className="text-[color:var(--mui-color-primary-main)]">하하하</span>
     </div>
   );
 }
